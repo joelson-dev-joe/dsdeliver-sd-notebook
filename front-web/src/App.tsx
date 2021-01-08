7fbb62b97535d76c237f48eeeff7a06d4dd45bd0
joelson-dev-joe <joelson.dh@gmail.com>
2021-01-08 11:53:55 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './home';
 import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ front-web/src/home/index.tsx
@@ -0,0 +1,21 @@
+import './styles.css';
+import { ReactComponent as MainImage } from './main.svg';
+
+function Home (){
+    return(
+   <div className="home-container">
+       <div className="home-content">
+           <div className="home-actions">
+               <h1 className="home-title">Faça seu pedido <br/> que entregamos <br/> para você</h1>
+               <h3 className="homesubtitle">Escolha seu pedido <br/> em poucos minutos levaremos a sua porta</h3>
+               <a href="orders" className="home-btn-order">FAZER PEDIDO</a>
+           </div>
+           <div className="home-image">
+               <MainImage />
+           </div>
+       </div>
+   </div>
+    )
+}
+
+export default Home;
